e8f786d6aaa55d107f763af8eed55ee36598476f
Ollanta CG <ollanta@elsa.science>
2023-12-17 12:50:17 +0100

    Cleaner day17?

--- day17/two.hs
+++ day17/two.hs
@@ -2,7 +2,6 @@ import Text.Parsec
 import Parsing
 import Chart2d
 import qualified Data.HashMap.Strict as M
-import Searches
 import qualified Data.Heap as H
 
 main :: IO ()
@@ -27,48 +26,43 @@ solve input = unlines [
     find chart = minsearch heapinit meminit
       where
         heapinit :: H.MinPrioHeap Integer (Coord, Integer, Char, [Coord])
-        heapinit = H.fromList . zip (repeat 0) $ [((0,0),10,'R',[]),((0,0),10,'D',[])]
+        heapinit = H.fromList . zip (repeat 0) $ [((0,0),0,'D',[]),((0,0),0,'R',[])]
 
         meminit = M.empty
 
-        end ((x, y),_,_) = (x,y) == (mx,my)
+        end c = c == (mx,my)
 
-        moves (c,n,d)
-          | n < 4 = filter inChart [(moves' c d, n+1, d)]
-          | otherwise = filter inChart [(moves' c d', newN n d', d') | d' <- newDs d, newN n d' <= 10]
+        moves (c, n, d, p) = [(c', n', d', c:p) | (n', d') <- (n+1, d):zip (repeat 1) turns,
+                                                  let c' = move c d',
+                                                  inChart c',
+                                                  n' <= 10]
           where
-            newDs 'L' = "UDL"
-            newDs 'R' = "UDR"
-            newDs 'U' = "LRU"
-            newDs 'D' = "LRD"
+            turns
+              | n < 4 = []
+              | d `elem` "LR" = "UD"
+              | d `elem` "UD" = "LR"
 
-            newN n d'
-              | d == d' = n + 1
-              | otherwise = 1
+            move (x,y) 'L' = (x-1,y)
+            move (x,y) 'R' = (x+1,y)
+            move (x,y) 'U' = (x,y-1)
+            move (x,y) 'D' = (x,y+1)
 
-            moves' (x,y) 'L' = (x-1,y)
-            moves' (x,y) 'R' = (x+1,y)
-            moves' (x,y) 'U' = (x,y-1)
-            moves' (x,y) 'D' = (x,y+1)
-
-            inChart ((x,y),_,_) = x >= 0 && x <= mx && y >= 0 && y <= my
+            inChart (x,y) = x >= 0 && x <= mx && y >= 0 && y <= my
 
         minsearch heap mem
-          | end state = (heat, coord:path)
-          | state `M.member` mem = minsearch heap' mem
-          | otherwise = minsearch heap'' mem'
+          | end coord    = (heat, reverse $ coord:path)
+          | inMemo state = minsearch heap' mem
+          | otherwise    = minsearch heap'' mem'
           where
             Just (next, heap') = H.view heap
-            (heat, lstate) = next
-            (coord, n, d, path) = lstate
-            state = (coord, n, d)
+            (heat, state) = next
+            (coord, n, dir, path) = state
 
-            mem' = M.insert state True mem
+            inMemo (c, n, d, _) = (c, n, d) `M.member` mem
+            mem' = M.insert (coord, n, dir) True mem
 
-            newStates = filter (not . (`M.member` mem)) $ moves state
+            newStates = [state' | state'@(c, _, _, p) <- moves state,
+                                  not (inMemo state')]
+            newHeats = [heat + chart M.! c | (c, _, _, _) <- newStates]
 
-            newHeats = [heat + chart M.! c | (c,_,_) <- newStates]
-
-            newLstates = [(a,b,c,coord:path) | (a,b,c) <- newStates]
-
-            heap'' = H.union heap' $ H.fromList (zip newHeats newLstates)
+            heap'' = H.union heap' . H.fromList $ zip newHeats newStates
